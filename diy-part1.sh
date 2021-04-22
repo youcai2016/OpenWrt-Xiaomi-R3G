db7af52ce5e67cd34d1948910e8b0a411e76a9d6
youcai2016 <45659413+youcai2016@users.noreply.github.com>
2021-04-22 19:49:09 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,4 +15,3 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
